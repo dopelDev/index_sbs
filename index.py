64163dd143edf56ec78f325132ab7d5218910a89
dopeldev <322kuroneko@gmail.com>
2023-06-25 04:07:31 -0500

    bulma has been isntalled

--- index.py
+++ index.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template
 
 index = Flask(__name__)
 
+services = ['Chat', 'Cloud', 'Jitsi', 'Minecraft', 'Mumble', 'Nextcloud', 'OpenVPN']
+
 @index.route('/')
-def index_page():
-    return render_template('index.html')
+def index_page(services = services):
+    return render_template('index.html', services=services)
 
 if __name__ == '__main__':
     index.run(debug=True)
